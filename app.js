8701616f44caf161dd7094694841e8627e62d3cd
Adrian Gil Gaspar <57201000169@utrng.edu.mx>
2022-06-13 22:22:23 -0500

    Redimencioné un canvas de 400x400 a traves de un boton

---
+++ app.js
@@ -0,0 +1,9 @@
+
+function agrandar()
+{
+const canvas=document.getElementById("lienzo")
+canvas.width = window.innerWidth
+canvas.height= window.innerHeight
+document.getElementById("boton").style="diplay:none";
+
+}
